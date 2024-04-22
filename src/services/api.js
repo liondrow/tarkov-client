8868a5c23e085d589b28eb8739e303b9f79c65ed
liondrow <liondrow2@yandex.ru>
2024-04-22 15:25:50 +0300

    pohryukayte hohli

--- src/services/api.js
+++ src/services/api.js
@@ -1,41 +1,11 @@
 import axios from "axios";
-import router from "@/router/index.js";
 
 const baseUrl = import.meta.env.VITE_BASE_API
-const token = JSON.parse(localStorage.getItem('token'))
-const API = axios.create({
-    baseURL: baseUrl,
-    headers: {
-        "Content-Type": "application/json",
-    }
-})
-API.interceptors.request.use(
-    (config) => {
-        config.headers["Authorization"] = 'Bearer ' + token;
-        if (token) {
-            config.headers["Authorization"] = 'Bearer ' + token;
+const instance = axios.create({
+        baseURL: baseUrl,
+        headers: {
+            "Content-Type": "application/json",
         }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
-API.interceptors.response.use(
-    response => {
-        return response;
-    },
-    error => {
-        if(error.response) {
-            if(error.response.status === 401) {
-                localStorage.removeItem('token');
-                router.replace({
-                    path:'/login'
-                })
-            }
-            return Promise.reject(error.response.data);
-        }
-    }
+});
 
-)
-export default API;
+export default instance;
