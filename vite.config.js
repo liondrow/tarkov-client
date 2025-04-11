2387cdd9fa791fcb971cbcc1eb8f0324c7ffba76
I.Sereda <isereda@iikosoftware.com>
2025-04-11 04:49:49 +0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    preview: {
+      allowedHosts: ['tarkov-anubis.ru'],
+    },
+  },
 })
